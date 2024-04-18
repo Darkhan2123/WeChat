80f47aa1fbbfb4a1003f50b72e8436bac701c8b4
QabylzhaparovAlinur <alinurkabylzhaparov@gmail.com>
2024-04-18 19:32:45 +0500

    back

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView, HomeView, ChatView
+from .views import RegisterView, LoginView, LogoutView, UserSearchView, AvailableUsersView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('user/<int:id>/', UserView.as_view(), name='user'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('home/', HomeView.as_view(), name='home'),
-    path('chat/', ChatView.as_view(), name='chat'),
+    path('search_user/', UserSearchView.as_view(), name='search_user'),
+    path('users/', AvailableUsersView.as_view(), name='users'),
 ]
